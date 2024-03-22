e3f89d62591c9ad9af943ffb988946bbf874883a
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2024-03-22 10:13:10 +0100

    refactor: Use blueprint-compiler

--- update-files.sh
+++ update-files.sh
@@ -1,4 +1,5 @@
 cd Noiseclapper@JordanViknar
 glib-compile-schemas schemas
+blueprint-compiler compile prefs.blp > prefs.ui
 xgettext --from-code=UTF-8 --output=locale/Noiseclapper.pot *.js *.ui
 cd ..
